fab6096786115bce65b7b4611d01be690c5a999f
nikolajjuuel <nikolaj.juuel@gmail.com>
2021-10-16 17:45:35 +0000

    data base clean up and file name change

--- db/seeds/02_tasks.sql
+++ db/seeds/02_tasks.sql
@@ -1,15 +1,10 @@
--- Widgets table seeds here (Example)
--- INSERT INTO widgets (name, user_id) VALUES ('Sprockets', 1);
--- INSERT INTO widgets (name, user_id) VALUES ('Chains', 2);
--- INSERT INTO widgets (name, user_id) VALUES ('Bearings', 2);
+-- Tasks table seeds here (Example)
 
 INSERT INTO tasks (user_id, title, category) VALUES (1, 'Of Mice and Men','To read');
 INSERT INTO tasks (user_id, title, category) VALUES (1, 'the circle','To watch');
 INSERT INTO tasks (user_id, title, category) VALUES (1, 'hotdogs','To eat');
 INSERT INTO tasks (user_id, title, category) VALUES (1, 'mustard','To buy');
-
 INSERT INTO tasks (user_id, title, category) VALUES (2, 'stanger things','To watch');
 INSERT INTO tasks (user_id, title, category) VALUES (2, 'big mac','To eat');
-
 INSERT INTO tasks (user_id, title, category) VALUES (3, 'wool socks','To buy');
 INSERT INTO tasks (user_id, title, category) VALUES (3, 'how to stay warm','To read');
