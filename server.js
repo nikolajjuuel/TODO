04176d93f49fd66d4d3c2024934e9f8d06026959
shivangipatodiya <shivu2610@gmail.com>
2021-10-18 17:00:51 +0000

    autocategorization

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 var cookieSession = require("cookie-session");
+// Required for API call
+var axios = require("axios");
+var convert = require("xml-js");
 
 app.use(
   cookieSession({
@@ -64,6 +67,22 @@ app.use("/api/tasks", tasksRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
+///////////////////////////////////////////////////////////////
+/////CATEGORIZE ME FUNCTION////////////////////////////////
+
+const categorizeMe = (text) => {
+  if (text.toLowerCase().includes("food")) {
+    return "To eat";
+  } else if (text.toLowerCase().includes("movie")) {
+    return "To watch";
+  } else if (text.toLowerCase().includes("book")) {
+    return "To read";
+  }
+  return "Not categorized by wolfram";
+};
+
+
+///////////////////////////////////////////
 app.get("/login/:id", (req, res) => {
   req.session.user_id = req.params.id;
 
@@ -92,26 +111,101 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/text", (req, res) => {
-
+app.post("/text", async (req, res) => {
   //checking to make sure someone is logged in
-  if(req.session.user_id === undefined){
-    return res.status(400).send('Need to login first')
+  if (req.session.user_id === undefined) {
+    return res.status(400).send("Need to login first");
   }
-   const id = req.session.user_id
-   const text = req.body.text;
-   const category = 'To watch';
+  const id = req.session.user_id;
+  const text = req.body.text;
+  //const category = "To watch";
+  //////////////////////////////////////////////////////////
+  let config = {
+    method: "get",
+    url: `https://api.wolframalpha.com/v2/query?input=${encodeURIComponent(
+      text
+    )}&appid=E7VRV7-28KWR98T9A`,
+  };
 
-   //Added to database
-   db.query(`
+  axios(config)
+    .then(function (response) {
+      console.log("============================");
+      let answer = convert.xml2js(response.data);
+      console.log("##################################");
+      let answeredCategory = answer.elements[0].attributes.datatypes;
+      console.log("ANSWER", answeredCategory);
+      let category = categorizeMe(answeredCategory)
+      //Added to database
+  db.query(
+    `
    INSERT INTO tasks (user_id, title, category)
    VALUES ($1, $2, $3);
-   `, [id, text, category])
-   .then((data) => {
-    res.redirect("/");
+   `,
+    [id, text, category]
+  )
+    .then((data) => {
+      res.redirect("/");
     })
-    .catch((err) => {console.log(err)})
-  });
+    .catch((err) => {
+      console.log(err);
+    });
+
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  ///////////////////////////////////////////////////////////
+/////////////////////////////////////////////
+  //Added to database
+  // db.query(
+  //   `
+  //  INSERT INTO tasks (user_id, title, category)
+  //  VALUES ($1, $2, $3);
+  //  `,
+  //   [id, text, category]
+  // )
+  //   .then((data) => {
+  //     res.redirect("/");
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+
+  /////////////////////////////////////////////////////////////////
+  //const whatToDo = req.body.text;
+
+  // var config = {
+  //   method: 'get',
+  //   url: `https://api.wolframalpha.com/v2/query?input=${encodeURIComponent("ramen")}&appid=E7VRV7-28KWR98T9A`,
+
+  // };
+
+  // axios(config)
+  // .then(function (response) {
+  //   console.log('============================')
+  //   let answer = convert.xml2js(response.data)
+  //   console.log('##################################')
+  //   console.log('ANSWER', answer.elements[0].attributes.datatypes)
+  // })
+  // .catch(function (error) {
+  //   console.log(error);
+  // });
+
+  //changing whatToDo so its readable by the openlirary API
+  // const whatToRead = whatToDo.trim().split(' ').join('+');
+
+  // axios.get(`http://openlibrary.org/search.json?title=${whatToRead}`)
+
+  // // Promise.all([promise1]).then((values) => {
+  // //   console.log(values[0].data.docs);
+  // // })
+  // .then((res) => {
+  //   console.log(res.data.docs)
+  //   });
+
+  // res.redirect("/");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
