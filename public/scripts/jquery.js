46cd53497edb520ba1d44880c8b905a09b608bf5
thomas <tmeyerwebdev@gmail.com>
2021-10-19 18:35:32 +0000

    add load spinner

--- public/scripts/jquery.js
+++ public/scripts/jquery.js
@@ -1,53 +1,57 @@
 $(function () {
-   const checkExist = setInterval(function () {
+  $("#submitButton").on("click", function () {
+    $("#form").css("display", "none");
+    $(".lds-ring").css("display", "inline-block");
+  });
 
-      $('.edit').on("click", function () {
-         const $options = $('.active');
-        $(this).siblings('.categories').slideToggle();
-      })
+  const checkExist = setInterval(function () {
+    $(".edit").on("click", function () {
+      const $options = $(".active");
+      $(this).siblings(".categories").slideToggle();
+    });
 
-      if ($(".fa-times")) {
-         console.log("Exists!");
+    if ($(".fa-times")) {
+      console.log("Exists!");
 
-         $(".fa-times").on("click", function () {
-            $(this).parent().parent().toggleClass("clicked");
+      $(".fa-times").on("click", function () {
+        $(this).parent().parent().toggleClass("clicked");
 
-            const id = $(this).prev()[0].getAttribute("data-task-id"); //id = task id
+        const id = $(this).prev()[0].getAttribute("data-task-id"); //id = task id
 
-            $.ajax({
-               url: `/delete/${id}`, //id = task id
-               method: "POST",
-               success: function (data) {
-                  console.log('&&&&&&&&&&&', id)
-                  location.reload();
-               },
-               error: function (err) {
-                  console.error(err);
-               },
-            });
-         });
+        $.ajax({
+          url: `/delete/${id}`, //id = task id
+          method: "POST",
+          success: function (data) {
+            console.log("&&&&&&&&&&&", id);
+            location.reload();
+          },
+          error: function (err) {
+            console.error(err);
+          },
+        });
+      });
 
-        //  if ($('#watch-edit')) {
-        //   console.log("YOU CAN EDIT TO WATCH")
-        //   $("#watch-edit").on("click", function (e) {
-        //     // e.preventDefault()
-        //     const id = $(this).parent().parent().children().children()[1].getAttribute("data-task-id");
-        //     console.log("@@@@@@@@@@@@@@ task id", id)
+      //  if ($('#watch-edit')) {
+      //   console.log("YOU CAN EDIT TO WATCH")
+      //   $("#watch-edit").on("click", function (e) {
+      //     // e.preventDefault()
+      //     const id = $(this).parent().parent().children().children()[1].getAttribute("data-task-id");
+      //     console.log("@@@@@@@@@@@@@@ task id", id)
 
-        //     $.ajax({
-        //       url: `/edit/${id}`, //id = task id
-        //       method: "POST",
-        //       success: function (data) {
-        //          console.log("@@@@@@@@@@@@ success")
-        //          //location.reload();
-        //       },
-        //       error: function (err) {
-        //          console.error(err);
-        //       },
-        //    });
-        //});
+      //     $.ajax({
+      //       url: `/edit/${id}`, //id = task id
+      //       method: "POST",
+      //       success: function (data) {
+      //          console.log("@@@@@@@@@@@@ success")
+      //          //location.reload();
+      //       },
+      //       error: function (err) {
+      //          console.error(err);
+      //       },
+      //    });
+      //});
       //}
-         clearInterval(checkExist);
-      }
-   }, 100); // check every 100ms
+      clearInterval(checkExist);
+    }
+  }, 100); // check every 100ms
 });
