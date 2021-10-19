f69932aae7fb75e05799eb88bb6b6d47ae055a52
dhawalrank <sojitradhawal@gmail.com>
2021-10-19 12:48:48 -0600

    WIP reload changes

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -19,26 +19,27 @@ $(function () {
 
     createTaskElement = function (obj) {
         console.log(obj)
-
-        if (obj.category === 'To watch') {
-            const $watch = $('#toWatch');
-            const $task = $(`      <div class="container">
+        const $watch = $("#toWatch");
+        const $eat = $('#toEat');
+        const $read = $('#toRead');
+        const $buy = $('#toBuy');
+        const $task = $(`<div class="container">
             <header><i class="fas fa-compact-disc main"></i></i>
               <div data-task-id=${obj.id} class="title">${obj.title}</div><i class="fas fa-times"></i>
             </header>
             <hr>
             <div class="categories" style="display:none">
-              <form action="/edit/${obj.id}" method="POST">
-              <button type="submit" name="category" value="To watch"><i class="fas fa-compact-disc">
+              <form class="switch-category" data-task-id=${obj.id}>
+              <button type="submit" name="To watch" value="To watch"><i class="fas fa-compact-disc">
                     <p>Watch</p>
                   </i></button>
-                <button name="category" type="submit" value="To read"> <i class="fas fa-book">
+                <button name="To read" type="submit" value="To read"> <i class="fas fa-book">
                     <p>Read</p>
                   </i></button>
-                <button name="category" type="submit" value="To eat"><i class="fas fa-pizza-slice">
+                <button name="To eat" type="submit" value="To eat"><i class="fas fa-pizza-slice">
                     <p>Eat</p>
                   </i></button>
-                <button name="category" type="submit" value="To buy"><i class="fas fa-shopping-cart">
+                <button name="To buy" type="submit" value="To buy"><i class="fas fa-shopping-cart">
                     <p>Buy</p>
                   </i></button>
               </form>
@@ -48,99 +49,16 @@ $(function () {
               <div>Change Category</div><i class="far fa-edit"></i>
             </div>
           </div>`);
-
+        if (obj.category === 'To watch') {
             return $watch.append($task);
         }
-        if (obj.category === 'To eat') {
-            const $eat = $('#toEat');
-            const $task = $(`     <div class="container">
-            <header><i class="fas fa-pizza-slice main"></i></i>
-              <div data-task-id=${obj.id} class="title">${obj.title}</div><i class="fas fa-times"></i>
-            </header>
-            <hr>
-            <div class="categories" style="display:none">
-              <form action="/edit/${obj.id}" method="POST">
-                <button name="category" type="submit" value="To watch"><i class="fas fa-compact-disc">
-                    <p>Watch</p>
-                  </i></button>
-                <button name="category" type="submit" value="To read"> <i class="fas fa-book">
-                    <p>Read</p>
-                  </i></button>
-                <button name="category" type="submit" value="To eat"><i class="fas fa-pizza-slice">
-                    <p>Eat</p>
-                  </i></button>
-                <button name="category" type="submit" value="To buy"><i class="fas fa-shopping-cart">
-                    <p>Buy</p>
-                  </i></button>
-              </form>
-
-            </div>
-            <div class="edit">
-              <div>Change Category</div><i class="far fa-edit"></i>
-            </div>
-          </div>`);
-            return $eat.append($task);
+        else if (obj.category === 'To eat') {
+           return $eat.append($task);
         }
         if (obj.category === 'To read') {
-            const $read = $('#toRead');
-            const $task = $(`     <div class="container">
-            <header><i class="fas fa-book main"></i></i>
-              <div data-task-id=${obj.id} class="title">${obj.title}</div><i class="fas fa-times"></i>
-            </header>
-            <hr>
-            <div class="categories" style="display:none">
-              <form action="/edit/${obj.id}" method="POST">
-                <button name="category" type="submit" value="To watch"><i class="fas fa-compact-disc">
-                    <p>Watch</p>
-                  </i></button>
-                <button name="category" type="submit" value="To read"> <i class="fas fa-book">
-                    <p>Read</p>
-                  </i></button>
-                <button name="category" type="submit" value="To eat"><i class="fas fa-pizza-slice">
-                    <p>Eat</p>
-                  </i></button>
-                <button name="category" type="submit" value="To buy"><i class="fas fa-shopping-cart">
-                    <p>Buy</p>
-                  </i></button>
-              </form>
-
-            </div>
-            <div class="edit">
-              <div>Change Category</div><i class="far fa-edit"></i>
-            </div>
-          </div>`);
-
             return $read.append($task);
         }
         if (obj.category === 'To buy') {
-            const $buy = $('#toBuy');
-            const $task = $(`     <div class="container">
-            <header><i class="fas fa-shopping-cart main"></i></i>
-              <div data-task-id=${obj.id} class="title">${obj.title}</div><i class="fas fa-times"></i>
-            </header>
-            <hr>
-            <div class="categories" style="display:none">
-              <form action="/edit/${obj.id}" method="POST">
-                <button  name="category" id="watch-edit" name="${obj.id}" type="submit" value="To watch"><i class="fas fa-compact-disc">
-                    <p>Watch</p>
-                  </i></button>
-                <button name="category" type="submit" value="To read"> <i class="fas fa-book">
-                    <p>Read</p>
-                  </i></button>
-                <button name="category" type="submit" value="To eat"><i class="fas fa-pizza-slice">
-                    <p>Eat</p>
-                  </i></button>
-                <button name="category" type="submit" value="To buy"><i class="fas fa-shopping-cart">
-                    <p>Buy</p>
-                  </i></button>
-              </form>
-
-            </div>
-            <div class="edit">
-              <div>Change Category</div><i class="far fa-edit"></i>
-            </div>
-          </div>`);
-
             return $buy.append($task);
         }
 
--- public/scripts/jquery.js
+++ public/scripts/jquery.js
@@ -1,53 +1,73 @@
 $(function () {
-   const checkExist = setInterval(function () {
+  const checkExist = setInterval(function () {
+    $(".edit").on("click", function () {
+      const $options = $(".active");
+      $(this).siblings(".categories").slideToggle();
+    });
 
-      $('.edit').on("click", function () {
-         const $options = $('.active');
-        $(this).siblings('.categories').slideToggle();
-      })
+    if ($(".fa-times")) {
+      console.log("Exists!");
 
-      if ($(".fa-times")) {
-         console.log("Exists!");
+      $(".fa-times").on("click", function () {
+        $(this).parent().parent().toggleClass("clicked");
 
-         $(".fa-times").on("click", function () {
-            $(this).parent().parent().toggleClass("clicked");
+        const id = $(this).prev()[0].getAttribute("data-task-id"); //id = task id
 
-            const id = $(this).prev()[0].getAttribute("data-task-id"); //id = task id
-
-            $.ajax({
-               url: `/delete/${id}`, //id = task id
-               method: "POST",
-               success: function (data) {
-                  console.log('&&&&&&&&&&&', id)
-                  location.reload();
-               },
-               error: function (err) {
-                  console.error(err);
-               },
-            });
-         });
-
-        //  if ($('#watch-edit')) {
-        //   console.log("YOU CAN EDIT TO WATCH")
-        //   $("#watch-edit").on("click", function (e) {
-        //     // e.preventDefault()
-        //     const id = $(this).parent().parent().children().children()[1].getAttribute("data-task-id");
-        //     console.log("@@@@@@@@@@@@@@ task id", id)
-
-        //     $.ajax({
-        //       url: `/edit/${id}`, //id = task id
-        //       method: "POST",
-        //       success: function (data) {
-        //          console.log("@@@@@@@@@@@@ success")
-        //          //location.reload();
-        //       },
-        //       error: function (err) {
-        //          console.error(err);
-        //       },
-        //    });
-        //});
-      //}
-         clearInterval(checkExist);
+        $.ajax({
+          url: `/delete/${id}`, //id = task id
+          method: "POST",
+          success: function (data) {
+            console.log("&&&&&&&&&&&", id);
+            location.reload();
+          },
+          error: function (err) {
+            console.error(err);
+          },
+        });
+      });
+      if ($(".switch-category")) {
+        $(".switch-category").on("submit", function (e) {
+          e.preventDefault();
+          const id = $(this).attr("data-task-id"); //task id
+          const newCategory = $(this)
+            .find("button[type=submit]:focus")[0]
+            .getAttribute("name");
+          $.ajax({
+            url: `/edit/${id}`, //id = task id
+            data: { category: newCategory },
+            method: "POST",
+            success: function (data) {
+              console.log("@@@@@@@@@@@@ success");
+              location.reload();
+            },
+            error: function (err) {
+              console.error(err);
+            },
+          });
+        });
       }
-   }, 100); // check every 100ms
+
+      //  if ($('#watch-edit')) {
+      //   console.log("YOU CAN EDIT TO WATCH")
+      //   $("#watch-edit").on("click", function (e) {
+      //     // e.preventDefault()
+      //     const id = $(this).parent().parent().children().children()[1].getAttribute("data-task-id");
+      //     console.log("@@@@@@@@@@@@@@ task id", id)
+
+      //     $.ajax({
+      //       url: `/edit/${id}`, //id = task id
+      //       method: "POST",
+      //       success: function (data) {
+      //          console.log("@@@@@@@@@@@@ success")
+      //          //location.reload();
+      //       },
+      //       error: function (err) {
+      //          console.error(err);
+      //       },
+      //    });
+      //});
+      //}
+      clearInterval(checkExist);
+    }
+  }, 100); // check every 100ms
 });
