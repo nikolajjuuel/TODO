c69b32a26201525253de17dbf62e8cba51fe680b
thomas <tmeyerwebdev@gmail.com>
2021-10-19 19:34:04 +0000

    update to buy filter

--- helpers/task.js
+++ helpers/task.js
@@ -1,12 +1,18 @@
 module.exports = {
   categorizeMe(text) {
+
+    console.log('text>>>>>>>>>', text);
+
+
     if (text.toLowerCase().includes("food")) {
       return "To eat";
     } else if (text.toLowerCase().includes("movie")) {
       return "To watch";
     } else if (text.toLowerCase().includes("book")) {
       return "To read";
+    } else {
+      return "To buy";
     }
-    return "Not categorized by wolfram";
   },
 };
+
