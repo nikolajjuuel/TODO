64a77874125d15147fc59f996b5db3f89e6eea14
thomas <tmeyerwebdev@gmail.com>
2021-10-20 02:37:32 +0000

    add img url and more info about task to table

--- db/schema/02_tasks.sql
+++ db/schema/02_tasks.sql
@@ -5,5 +5,7 @@ CREATE TABLE tasks (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
   title VARCHAR(255) NOT NULL,
-  category VARCHAR (255) NOT NULL
+  category VARCHAR (255) NOT NULL,
+  task_img_url VARCHAR (65535) NOT NULL,
+  task_text_info VARCHAR (65535) NOT NULL
 );
