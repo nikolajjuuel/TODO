f2d2a58f2a1fec9970939cf4ff32b326ec54833b
nikolajjuuel <nikolaj.juuel@gmail.com>
2021-10-21 20:17:56 +0000

    pair programming linting routes

--- routes/text.js
+++ routes/text.js
@@ -4,65 +4,47 @@ const axios = require("axios");
 const taskHelper = require("../helpers/task");
 const descriptionImage = require("../helpers/descriptionFinder")
 
-
-
-
-
 module.exports = (db) => {
   router.post("/", (req, res) => {
-    //checking to make sure someone is logged in
+    //making sure someone is logged in
     if (req.session.user_id === undefined) {
       return res.status(400).send("Need to login first");
     }
+
     const id = req.session.user_id;
     const text = req.body.text.trim();
+
     if (!text) {
       return res.redirect("/");
     }
 
+    const serpstackApi = process.env.SEPRSTACKKEY
     const wolfApi = process.env.WOLFAPIKEY;
     const encode = encodeURIComponent(text);
-    const url = `https://api.wolframalpha.com/v2/query?input=${encode}&output=JSON&appid=${wolfApi}`;
+    const urlWolf = `https://api.wolframalpha.com/v2/query?input=${encode}&output=JSON&appid=${wolfApi}`;
+    const urlSerp = `https://serpapi.com/search.json?engine=google&q=${encode}&google_domain=google.com&tbm=isch&ijn=0&api_key=${serpstackApi}`;
 
-    axios.get(url).then(function (response) {
+    axios.get(urlWolf).then(function (response) {
       const answer = response.data;
       const answerInformation = answer.queryresult;
       const answeredCategory = answer.queryresult.datatypes;
-      const plainTextInfo = descriptionImage.descriptionFinder(answerInformation);
-      console.log('SEARCH INFO', plainTextInfo)
-      const serpstackApi = process.env.SEPRSTACKKEY
-      //const answerImg = answerInformation.pods[2].subpods[0].img.src;
+      //finds description image using helper function descriptionFinder
+      const infoImage = descriptionImage.descriptionFinder(answerInformation);
       axios
-        .get(
-          `https://serpapi.com/search.json?engine=google&q=${encode}&google_domain=google.com&tbm=isch&ijn=0&api_key=${serpstackApi}`
-        )
+        .get(urlSerp)
         .then((resp) => {
-          const answer = resp;
-          console.log("IMG RESPONSE", resp.data.images_results[0].original);
+          //selects first image from serp api
           let answerImg = resp.data.images_results[0].original;
-
-          const podsArray = answerInformation.pods[2].subpods;
-
-          //console.log(answerInformation)
           const category = taskHelper.categorizeMe(answeredCategory);
-          //Added to database
-          // const formatMe = (text) => {
-          //   const x = text.split("\n");
-          //   console.log('TEXT IS',)
-          //   const ans = text[0] + '\n' + text[1];
-
-          //   return ans;
-          // };
-          // console.log(formatMe(plainTextInfo), "THIS IS IT");
-
           db.query(
             `
-   INSERT INTO tasks (user_id, title, category, task_img_url, task_text_info)
-   VALUES ($1, $2, $3, $4, $5);
-   `,
-            [id, text, category, answerImg, plainTextInfo]
+             INSERT INTO tasks 
+             (user_id, title, category, task_img_url, task_text_info)
+             VALUES ($1, $2, $3, $4, $5);
+            `,
+            [id, text, category, answerImg, infoImage]
           )
-            .then((data) => {
+            .then(() => {
               res.redirect("/");
             })
             .catch((err) => {
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,12 @@
-/*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into api/users,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
-
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
+    db.query(`SELECT * FROM users
+    WHERE id = ${req.session.user_id}
+    ORDER BY id DESC;
+    ;`)
       .then(data => {
         const users = data.rows;
         res.json({ users });
